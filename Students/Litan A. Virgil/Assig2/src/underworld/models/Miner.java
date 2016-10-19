a00469ac295718cc685d5fb74733736d686e502b
Litan Virgil <virgilx13@gmail.com>
2016-10-19 22:47:19 +0300

    minor corrections

--- Students/Litan A. Virgil/Assig2/src/underworld/models/Miner.java
+++ Students/Litan A. Virgil/Assig2/src/underworld/models/Miner.java
@@ -10,6 +10,7 @@ public class Miner {
 	
 	public Miner(String name){
 		backpack = new int[Constants.MINER_BACKPACK_SIZE];
+		backpackIndex = 0;
 		this.name = name;
 		for (int i = 0; i < Constants.MINER_BACKPACK_SIZE; i++){
 			backpack[i] = -1;
@@ -21,23 +22,18 @@ public class Miner {
 			return (-1);
 		}
 		else{
-			for (int i = 0; i < Constants.MINER_BACKPACK_SIZE; i++){
-				if (backpack[i] == -1){//find empty slot
-					backpack[i] = Resources.generateResource();
-					return (backpack[i]);
-				}
-			}
+			backpack[backpackIndex++] = Resources.generateResource();
+			return (backpack[backpackIndex - 1]);
 		}
-		//should never be reached
-		return (-2);
 	}
+	
 	public int sleep(){
 		return (Resources.generateResource());
 	}
+	
 	public void showBackpack(){
 		System.out.println("[Miner] The backpack of miner '" + this.name +"' contains:");
-		for (int i = 0; i < Constants.MINER_BACKPACK_SIZE; i++)
-		{
+		for (int i = 0; i < Constants.MINER_BACKPACK_SIZE; i++){
 			System.out.println("[Miner] in slot " + i + " the resource " + backpack[i]);
 		}
 		System.out.println("[Miner] --------end backpack--------");
@@ -46,22 +42,16 @@ public class Miner {
 	public String getName(){
 		return (this.name);
 	}
+	
 	public void	setName(String name){
 		this.name = name;
 	}
 	//private
 	private boolean isBackpackFull(){
-		return (getNumberOfBackpackSlots() == 0);
+		return (getNumberOfBackpackSlots() <= 0);
 	}
 	
 	private int getNumberOfBackpackSlots(){
-		int emptySlots = 0;
-		
-		for (int i = 0; i < Constants.MINER_BACKPACK_SIZE; i++){
-			if (backpack[i] == -1){
-				emptySlots++;
-			}
-		}
-		return (emptySlots);
+		return (Constants.MINER_BACKPACK_SIZE - backpackIndex);
 	}
 }
